3036bc878fa62a0dc4beca5e762e78b4a258d378
Hubert Kaczorowski <h.kaczorowski@stud.elka.pw.edu.pl>
2021-06-23 21:19:37 +0000

    Update OnePostController.java

--- Widoki/OnePostController.java
+++ Widoki/OnePostController.java
@@ -1,1 +1,28 @@
+package Widoki;
 
+import Dane.News;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class OneNewsController {
+
+    @FXML
+    private Label tytul;
+
+    @FXML
+    private Label tresc;
+
+    @FXML
+    private Button czytaj;
+
+    private PostNaForum post;
+
+    public void setData(PostNaForum post){
+        this.post = post;
+        tytul.setText(post.getNazwa());
+        tresc.setText(post.getTresc());
+
+    
+    }
+}
